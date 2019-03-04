74d76bbc7fd0282a31667de174b76c4d6f4d712e
Archer <arseniy.tarapon@gmail.com>
2019-03-04 14:58:53 +0600

    В файл api_v1/views.py импортировал модели Category, Hall, Seat, Show, и сериализаторы CategorySerializer, HallSerializer, SeatSerializer, ShowSerializer. Создал класс ViewSet для каждой модели и соответствующего сериализатора.

--- cinema/api_v1/views.py
+++ cinema/api_v1/views.py
@@ -1,9 +1,29 @@
-from webapp.models import Movie
+from webapp.models import Movie, Category, Hall, Seat, Show
 from rest_framework import viewsets
-from api_v1.serializers import MovieSerializer
+from api_v1.serializers import MovieSerializer, CategorySerializer, HallSerializer, SeatSerializer, ShowSerializer
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all().order_by('-release_date')
     serializer_class = MovieSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class HallViewSet(viewsets.ModelViewSet):
+    queryset = Hall.objects.all()
+    serializer_class = HallSerializer
+
+
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+
+class ShowViewSet(viewsets.ModelViewSet):
+    queryset = Show.objects.all()
+    serializer_class = ShowSerializer
 

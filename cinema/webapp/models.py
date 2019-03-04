4f6df03be09007154f4f950fe8fa4fbccb951cad
Archer <arseniy.tarapon@gmail.com>
2019-03-04 13:49:18 +0600

    Добавил файл serializers.py для сериализации и десериализации данных. Добавил MovieSerializer в него. В файл views.py в приложении api_v1 импортировал объект viewsets из библиотеки rest_framework. Так же импортировал сериализатор MovieSerializer из serializers.py. Добавил класс MovieViewSet. Импортировал наши приложение в settings.py. В приложении api_v1 добавил файл urls.py. Редактировал основной urls.py, .gitignore и requirements.txt. Добавил следующие модели : Category, Hall, Seat, Show и зарегистрировал их в admin.py. Командами migrate и makemigrations создал и применил новые миграции.

--- cinema/webapp/models.py
+++ cinema/webapp/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ManyToManyField(Category)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    poster = models.ImageField(upload_to='posters', null=True, blank=True)
+    release_date = models.DateField()
+    finish_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Hall(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Seat(models.Model):
+    hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    def __str__(self):
+        return 'Ряд: %s, Место: %s' % (self.row, self.seat)
+
+
+class Show(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    hall = models.ForeignKey(Hall, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    finish_time = models.DateTimeField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.price

922e36cd7cbe4f4c3434c663887412b1e9c75f4a
Archer <arseniy.tarapon@gmail.com>
2019-03-25 16:32:17 +0600

    Передача дополнительных данных после входа. Проверка на то, что пользователь является админом в API.

--- cinema/api_v1/views.py
+++ cinema/api_v1/views.py
@@ -2,9 +2,27 @@ from webapp.models import Movie, Category, Hall, Seat, Show, Discount, Ticket, B
 from rest_framework import viewsets
 from api_v1.serializers import MovieSerializer, CategorySerializer, HallSerializer, SeatSerializer, \
     ShowSerializer, DiscountSerializer, TicketSerializer, BookSerializer, UserSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from django.contrib.auth.models import User
+from rest_framework.generics import CreateAPIView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'username': user.username,
+            'is_admin': user.is_superuser,
+            'is_staff': user.is_staff
+        })
 
 
 # Базовый класс ViewSet, основанный на ModelViewSet
@@ -16,7 +34,7 @@ class BaseViewSet(viewsets.ModelViewSet):
         # добавляем его объект ( IsAuthenticated() ) к разрешениям только
         # для "опасных" методов - добавление, редактирование, удаление данных.
         if self.request.method in ["POST", "DELETE", "PUT", "PATCH"]:
-            permissions.append(IsAuthenticated())
+            permissions.append(IsAuthenticated(),IsAdminUser())
         return permissions
 
 

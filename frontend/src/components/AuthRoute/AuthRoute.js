d13471eda6a70e0de0861ba38168b47033305125
Archer <arseniy.tarapon@gmail.com>
2019-04-06 01:34:05 +0600

    Исправлено обновление страниц, которые требуют авторизацию.

--- frontend/src/components/AuthRoute/AuthRoute.js
+++ frontend/src/components/AuthRoute/AuthRoute.js
@@ -4,18 +4,20 @@ import {connect} from "react-redux";
 
 
 const AuthRoute = (props) => {
+    if(props.app.loading) {
+        return <p>Loading, please wait.</p>
+    }
     if (props.auth.user_id) {
         return <Route {...props} />
-    } else {
-        return <Redirect to={{
-            pathname: "/login",
-            state: {next: props.location}
-        }}/>
     }
+    return <Redirect to={{
+        pathname: "/login",
+        state: {next: props.location}
+    }}/>
 };
 
 // вытаскиваем данные об аутентификации из state
-const mapStateToProps = state => ({auth: state.auth});
+const mapStateToProps = state => ({auth: state.auth, app: state.app});
 // никаких дополнительных действий здесь не нужно
 const mapDispatchToProps = dispatch => ({});
 

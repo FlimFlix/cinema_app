5c960c5c634cdbb7aee54b3ae30cfc367ef02e84
Archer <arseniy.tarapon@gmail.com>
2019-04-06 01:41:20 +0600

    MovieList переделан на Redux.

--- frontend/src/containers/MovieList/MovieList.js
+++ frontend/src/containers/MovieList/MovieList.js
@@ -1,26 +1,19 @@
 import React, {Fragment, Component} from 'react';
-import {MOVIES_URL} from "../../api-urls"
 import MovieCard from "../../components/MovieCard/MovieCard";
-import axios from 'axios';
+import {loadMovies} from "../../store/actions/movie-list";
+import {connect} from "react-redux";
 
 
 class MovieList extends Component {
-    state = {
-        movies: []
-    };
-
     componentDidMount() {
-        axios.get(MOVIES_URL)
-            .then(response => {console.log(response.data); return response.data;})
-            .then(movies => this.setState({movies}))
-            .catch(error => console.log(error));
+        this.props.loadMovies();
     }
 
 
     render() {
         return <Fragment>
             <div className='row'>
-            {this.state.movies.map(movie => {
+            {this.props.movies.map(movie => {
                return <div className='col-xs-12 col-sm-6 col-lg-4 mt-3' key={movie.id}>
                    <MovieCard movie={movie}/>
                </div>
@@ -30,4 +23,9 @@ class MovieList extends Component {
     }
 }
 
-export default MovieList;
+const mapStateToProps = (state) => state.movieList;
+const mapDispatchToProps = (dispatch) => ({
+    loadMovies: () => dispatch(loadMovies())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieList);
